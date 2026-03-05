666dfd4e78e1eeb23ce91bfcbeaf1ba9f37a28a3
mcrw <dan.mcrw13@gmail.com>
2026-03-05 18:45:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const audio = new Audio('Map/Mysterious Ways.mp3');
 audio.loop = true;
 audio.volume = 0.7;
-
+window.location.href = "trap.html";
 function playAudio() {
     audio.play().catch(err => {
         console.log("Autoplay blocked. User interaction required.", err);
